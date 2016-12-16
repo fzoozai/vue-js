5233cefbeb974a7ce1205620422190925f234538
fzoozai <fabian.soosaithasan@gmail.com>
2016-12-16 20:42:40 +0100

    Add another body element into tasks-prop

--- src/js/main.js
+++ src/js/main.js
@@ -38,7 +38,8 @@ new Vue({
         tasks: [
             { body:'Go to the store', completed: false },
             { body:'Go to the bank', completed: false },
-            { body:'Go to the cinema', completed: true }
+            { body:'Go to the cinema', completed: true },
+            { body:'Go to the mall', completed: true }
         ]
     },
 
